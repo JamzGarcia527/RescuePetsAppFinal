990f18d378c20d221a2b46f7d9c881bc91131e43
Jimmy Cantor <jimmy.cantor@thomasgreg.com>
2021-09-15 14:14:36 -0500

    Thread ok

--- app/src/main/java/com/jamzdeveploment/rescuepetsapp/SplashActivity.kt
+++ app/src/main/java/com/jamzdeveploment/rescuepetsapp/SplashActivity.kt
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(3000)
+
         super.onCreate(savedInstanceState)
         startActivity(Intent(this,SignInActivity::class.java))
     }
